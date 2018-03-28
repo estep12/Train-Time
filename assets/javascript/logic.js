1e91b559336694c69868d8da411cf049cde80984
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-03-27 20:51:51 -0500

    Added links to bootstrap, jQuery, and Firebase

---
+++ assets/javascript/logic.js
@@ -0,0 +1,11 @@
+
+  var config = {
+    apiKey: "AIzaSyDOOCcNAxbUoM1ivwlbetOQnAN6lBflBzM",
+    authDomain: "train-time-1316c.firebaseapp.com",
+    databaseURL: "https://train-time-1316c.firebaseio.com",
+    projectId: "train-time-1316c",
+    storageBucket: "",
+    messagingSenderId: "934413458247"
+  }; 
+  
+  firebase.initializeApp(config);
