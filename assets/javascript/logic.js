b2e05bcd2dccfc9f88bee9153e09924179111da2
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-03-29 21:28:12 -0500

    Appended the table rows with data. Data form user inputs is storing in firebase

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,30 +4,32 @@
     authDomain: "train-time-1316c.firebaseapp.com",
     databaseURL: "https://train-time-1316c.firebaseio.com",
     projectId: "train-time-1316c",
-    storageBucket: "",
+    storageBucket: "train-time-1316c.appspot.com",
     messagingSenderId: "934413458247"
   }; 
   
   firebase.initializeApp(config);
 
-  var database = firebase.database;
-
-  $("#add-train-btn").on("click", function(event){
+  var database = firebase.database();
+  
+  $("#add-train-button").on("click", function(event){
     event.preventDefault();
+    
 
     var trainName = $("#train-name").val().trim();
     var trainDest = $("#destination").val().trim();
-    var trainTime = moment($("#train-name").val().trim(), "hh:mm").format("X");
+    var trainTime = moment($("#train-time").val().trim(), "hh:mm").format("HH:mm");
     var trainFreq = $("#freq").val().trim();
 
-  });
+  
 
   var newTrain = {
         name: trainName,
         destination: trainDest,
         time: trainTime,
-        frequency: trainFreq
-  }
+        frequency: trainFreq,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+  };
 
   database.ref().push(newTrain);
 
@@ -35,7 +37,31 @@
   console.log(newTrain.destination);
   console.log(newTrain.time);
   console.log(newTrain.frequency);
-  
-  
-  
-  
+
+  $("#train-name").val("");
+  $("#destination").val("");
+  $("#train-time").val("");
+  $("#freq").val("");
+
+});
+
+
+database.ref().on("child_added", function(snapshot, prevKey){
+
+console.log(snapshot.val());
+
+var trainName = snapshot.val().name;
+var trainDest = snapshot.val().destination;
+var trainTime = snapshot.val().time;
+var trainFreq = snapshot.val().frequency;
+
+console.log(trainName);
+console.log(trainDest);
+console.log(trainTime);
+console.log(trainFreq);
+
+
+$(".table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDest + "</td><td>" + trainFreq + "</td></tr>")
+});
+
+
