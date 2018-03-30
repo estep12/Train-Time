a129b86c0b14c24e2d5f76cc5d3e3282cf91783e
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-03-29 19:56:58 -0500

    created varibles to grab inputs. Created object to push to Firebase database

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -9,3 +9,33 @@
   }; 
   
   firebase.initializeApp(config);
+
+  var database = firebase.database;
+
+  $("#add-train-btn").on("click", function(event){
+    event.preventDefault();
+
+    var trainName = $("#train-name").val().trim();
+    var trainDest = $("#destination").val().trim();
+    var trainTime = moment($("#train-name").val().trim(), "hh:mm").format("X");
+    var trainFreq = $("#freq").val().trim();
+
+  });
+
+  var newTrain = {
+        name: trainName,
+        destination: trainDest,
+        time: trainTime,
+        frequency: trainFreq
+  }
+
+  database.ref().push(newTrain);
+
+  console.log(newTrain.name);
+  console.log(newTrain.destination);
+  console.log(newTrain.time);
+  console.log(newTrain.frequency);
+  
+  
+  
+  
